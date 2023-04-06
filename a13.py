a4c5ca15bc3ad3473a28b6e0c3d2f3a6f30e4181
kuropenguin <kuro1994.t@gmail.com>
2023-04-07 05:53:10 +0900

    fix 尺取法

--- a13.py
+++ a13.py
@@ -1,5 +1,18 @@
-def shakutori(diff: int, list: list[int]) -> int:
-    return 10
+# list がsort済み前提
+# FIXME これ今 start_point がどんどん次に行ってしまっている（自分の差分の限界まで進んでから次のstart_pointに移るようにしたい）
+def shakutori(diff: int, sorted_list: list[int]) -> int:
+    count = 0
+    end_point = 1  # start_point の 次から確認を始める
+    for start_point in range(len(sorted_list) - 1):
+        while end_point < len(sorted_list) and sorted_list[end_point] - sorted_list[start_point] <= diff:
+            # start と end の間にある数は全て条件を満たすので、その数をカウントする
+            # 11, 13, 15 だった場合、 11 と 15 で条件を満たすのであれば, 12 と15 は必然的に条件を満たす(より近い距離にいるので)
+            count += end_point - start_point
+            # end_point を動かす
+            # end_point が len(sorted_list) になったら 何もしない（動かさない）
+            end_point += 1
+
+    return count
 
 
 def main():
